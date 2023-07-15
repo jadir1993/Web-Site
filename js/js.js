237339a73d35c3d968de06a1604a6088ae4a214c
jadir1993 <1895yair@gmail.com>
2023-07-14 22:28:12 -0500

    Agregando archivos formulario 2

--- js/js.js
+++ js/js.js
@@ -2,6 +2,7 @@ const hamburgerBtn = document.getElementById('hamburger-btn');
 const menu = document.getElementById('menu');
 const closeBtn = document.getElementById('close-btn');
 
+
 function seleccionarMenu() {
   if (menu.style.display === 'none' || menu.style.display === '') {
     menu.style.display = 'flex';
@@ -14,3 +15,30 @@ function seleccionarMenu() {
 
 hamburgerBtn.addEventListener('click', seleccionarMenu);
 closeBtn.addEventListener('click', seleccionarMenu);
+
+const form = document.querySelector('#formulario');
+
+form.addEventListener('submit', handlesubmit)
+
+async function handlesubmit(event) {
+  event.preventDefault()
+  const form = new FormData(this)
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form, 
+    headers:{
+      'Accept': 'aplication/json'
+    }
+  });
+  if (response.ok){
+      this.reset();
+      const alerta = document.getElementById('alerta');
+      alerta.classList.remove('hide');
+
+
+      // Elimina el elemento de alerta después de 3 segundos
+      setTimeout(() => {
+        alerta.classList.add('hide');
+      }, 3000);
+    }
+  }
